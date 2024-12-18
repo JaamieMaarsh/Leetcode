bfd3bd157e03e1f38cbd2ad2e652eb13ac9c7de5
Jaamie Maarsh Joy Martin <149022996+JaamieMaarsh@users.noreply.github.com>
2024-12-17 17:11:49 -0800

    Time: 12 ms (58.84%), Space: 20.5 MB (40.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,11 +3,12 @@ class Solution:
         hashmap = defaultdict(list)
         
         for s in strs:
-            sorted_key = "".join(sorted(s)) 
+            sorted_key = "".join(sorted(s)) #
             
             if sorted_key not in hashmap:
                 hashmap[sorted_key] = [s]
             else:
                 hashmap[sorted_key].append(s)
+                
         
         return list(hashmap.values())
