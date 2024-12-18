0d1e1631f95fae571e86f430c5cc4887e8e00fe7
Jaamie Maarsh Joy Martin <149022996+JaamieMaarsh@users.noreply.github.com>
2024-12-17 17:10:56 -0800

    Time: 11 ms (80.71%), Space: 20.4 MB (42.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,13 @@
-from collections import defaultdict
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        if strs is None:
-            return None
-        
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
         
         for s in strs:
-            count = [0] * 26 # creating an array holding the 26 characters
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            hashmap[tuple(count)].append(s)
-        return list(hashmap.values())
+            sorted_key = "".join(sorted(s)) 
+            
+            if sorted_key not in hashmap:
+                hashmap[sorted_key] = [s]
+            else:
+                hashmap[sorted_key].append(s)
         
+        return list(hashmap.values())
